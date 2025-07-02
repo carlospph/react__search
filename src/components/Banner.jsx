4ba03680259a264f00a5778f88f45aae9899b6c6
intablete <intablete@gmail.com>
2025-07-02 20:45:49 +0000

    Adicionando o formulario e os campos

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,18 +2,16 @@ import styled from 'styled-components';
 
 const Container = styled.div`
     margin: 0 0 1rem 0;
-
     & img{
         border-radius: 5px;
     }
 `;
 
-function Banner () {
-
-    return(
+function Banner() {
+    return (
         <Container>
-            <img src="https://weebpal.com/sites/default/files/styles/image_1920x690/public/2023-08/Hero-banner-2.jpg?itok=c4M23WDg" Alt="Banner"
-            className="banner"/>
+            <img src="https://weebpal.com/sites/default/files/styles/image_1920x690/public/2023-08/Hero-banner-2.jpg?itok=c4M23WDg" alt="Banner"
+                className="banner" />
         </Container>
     )
 }
