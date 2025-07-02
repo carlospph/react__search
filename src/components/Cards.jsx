f5767f4892c8b024e2bcfabadcf54d7b728f8b2b
intablete <intablete@gmail.com>
2025-07-02 05:36:09 +0000

    OK

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,36 +1,42 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
-  border:1px solid blue;
+  border: 1px solid blue;
+  max-width: 992px;
+  margin: 1rem auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+   gap: 20px;
 `;
 
 const Card = styled.div`
-  border:1px solid blue;
-`;
-
-const Card__body = styled.div`
-  padding: .7rem;
-`;
-
-const Title = styled.h4`
-  font-size:19px;
-`;
-
-const Category = styled.p`
-  font-size:15px;
-  line-height:150%;
-`;
-
-const Description = styled.p`
-  
+  border: 1px solid brown;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 `;
 
 const Cardbody = styled.div`
+  padding: 0.7rem;
+`;
 
+const Title = styled.h4`
+  font-size: 19px;
+`;
+
+const Category = styled.p`
+  font-size: 15px;
+  line-height: 150%;
+`;
+
+const Description = styled.p`
+  font-size: 14px;
+  line-height: 1.4;
 `;
 
 const Button = styled.button`
-
+  margin-top: auto;
+  padding: 0.5rem 1rem;
 `;
 
 function Cards() {
@@ -38,11 +44,33 @@ function Cards() {
     <Container>
       <Card>
         <Cardbody>
-          <Title>Tênis</Title>
+          <Title>Tênis Confort</Title>
           <Category>Calçado</Category>
           <Description>
             Tênis para quem gosta de calçar o que tem de melhor, para exigentes
-            e que busca conforto.
+            e que busca conforto. qualidade bem-estar e ammis paara qualquer hora do dia.
+          </Description>
+        </Cardbody>
+        <Button>Comprar</Button>
+      </Card>
+
+      <Card>
+        <Cardbody>
+          <Title>Tênis Urban Style</Title>
+          <Category>Calçado</Category>
+          <Description>
+            Conforto extremo com design moderno. Ideal para o dia a dia com muito estilo.
+          </Description>
+        </Cardbody>
+        <Button>Comprar</Button>
+      </Card>
+
+      <Card>
+        <Cardbody>
+          <Title>Camiseta Premium</Title>
+          <Category>Camisa</Category>
+          <Description>
+            Camiseta para quem não abre mão de estilo e leveza. Conforto e sofisticação em cada detalhe.
           </Description>
         </Cardbody>
         <Button>Comprar</Button>
