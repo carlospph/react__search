ea801377c68fc92cd79982988e46d47fc09fdac2
intablete <intablete@gmail.com>
2025-07-02 19:39:51 +0000

    Feito o form

--- src/components/ScreenSearch.jsx
+++ src/components/ScreenSearch.jsx
@@ -2,8 +2,6 @@ import Banner from './Banner';
  import FormCadastro from './FormCadastro';
 import Search from './Search';
 
- 
-
 function ScreenSearch() { 
   return (
     <div className="container">
